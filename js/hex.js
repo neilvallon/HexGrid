31a49765d231a9cb28d38579ccbf64f6ef263645
Neil Vallon <neilvallon@gmail.com>
2014-03-08 05:21:13 -0600

    Allow opening and closing sides of individual nodes.

--- js/hex.js
+++ js/hex.js
@@ -2,6 +2,20 @@ function HexNode() {
 	this.node = $("<div><div class='hexagon'></div></div>");
 };
 
+HexNode.prototype.sides =
+	["top-left", "top-right", "right", "bottom-right", "bottom-left", "left"];
+
+HexNode.prototype.open = function(side, row) {
+	var c = row? row + "-" : "";
+	c += side;
+
+	if(this.sides.indexOf(c) > -1) {
+		this.node.addClass(c);
+		return true;
+	}
+	return false;
+};
+
 function HexRow(n) {
 	this.nodes = [];
 	for(var i=0; i<n; i++) {
@@ -21,6 +35,10 @@ function HexGrid(x, y) {
 	}
 
 	this.html = this.hrows.map(function(r) { return r.html; });
+};
+
+HexGrid.prototype.find = function(x, y) {
+	return this.hrows[y].nodes[x];
 };
 
 $(function(){
