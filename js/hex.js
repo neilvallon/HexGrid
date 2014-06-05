0b56f9e02cd332ccb11f48ac1920036dafd48447
Neil Vallon <neilvallon@gmail.com>
2014-06-05 17:35:21 -0500

    Open adjacent nodes wall automatically

--- js/hex.js
+++ js/hex.js
@@ -3,28 +3,82 @@
  * Licensed under MIT (http://opensource.org/licenses/MIT)
  */
 
-function HexNode() {
-	this.node = $("<div><div class='hexagon'></div></div>");
-};
+function HexNode(x, y, row, grid) {
+	this.row = row;
+	this.grid = grid;
 
-HexNode.prototype.sides =
-	["top-left", "top-right", "right", "bottom-right", "bottom-left", "left"];
+	this.node = $("<div><div class='hexagon'></div></div>");
+
+	this.sides = {
+		"top-left": {"x":x-(!row.even), "y":y-1},
+		"top-right": {"x":x+row.even, "y":y-1},
+		"right": {"x":x+1, "y":y},
+		"bottom-right": {"x":x+row.even, "y":y+1},
+		"bottom-left": {"x":x-(!row.even), "y":y+1},
+		"left": {"x":x-1, "y":y}
+	};
+}
 
 HexNode.prototype.open = function(side, row) {
 	var c = row? row + "-" : "";
 	c += side;
 
-	if(this.sides.indexOf(c) > -1) {
-		this.node.addClass(c);
-		return true;
+	if(this.sides[c] === undefined) {
+		return false;
 	}
-	return false;
+
+	this.node.addClass(c);
+	return true;
 };
 
-function HexRow(n) {
+HexNode.prototype.openAdjacent = function(side, row) {
+	switch(side) {
+	case "left":
+		side = "right";
+		break;
+	case "right":
+		side = "left";
+		break;
+	default:
+		return false;
+	}
+	switch(row) {
+	case "top":
+		row = "bottom";
+		break;
+	case "bottom":
+		row = "top";
+		break;
+	}
+	return this.open(side, row);
+};
+
+HexNode.prototype.openWall = function(side, row) {
+	var c = row? row + "-" : "";
+	c += side;
+
+	var sideCord = this.sides[c];
+	if(sideCord === undefined) {
+		return false;
+	}
+
+	var sideNode = this.grid.find(sideCord.x, sideCord.y);
+	if(sideNode === undefined) {
+		return false;
+	}
+
+	this.node.addClass(c);
+	sideNode.openAdjacent(side, row);
+	return true;
+};
+
+function HexRow(y, n, grid) {
+	this.grid = grid;
+	this.even = y % 2 === 0;
+
 	this.nodes = [];
 	for(var i=0; i<n; i++) {
-		this.nodes.push(new HexNode);
+		this.nodes.push(new HexNode(i, y, this, grid));
 	}
 
 	this.html = $("<div class='hexrow'></div>")
@@ -36,12 +90,20 @@ function HexGrid(x, y) {
 
 	this.hrows = [];
 	for(var i=0; i<y; i++) {
-		this.hrows.push(new HexRow(x));
+		this.hrows.push(new HexRow(i, x, this));
 	}
 
 	this.html = this.hrows.map(function(r) { return r.html; });
 };
 
 HexGrid.prototype.find = function(x, y) {
-	return this.hrows[y].nodes[x];
+	var r = this.hrows[y];
+	if(r === undefined) {
+		return undefined
+	}
+
+	var n = r.nodes[x];
+	if(r !== undefined) {
+		return n
+	}
 };
